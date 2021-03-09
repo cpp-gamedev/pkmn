3c4c3855e9963b8ae6773eee7e6f760caabc5b35
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-03-09 15:51:37 +0100

    Implement range and random_choices

--- src/utils.hpp
+++ src/utils.hpp
@@ -5,6 +5,21 @@
 #include <vector>
 
 template <typename T>
+std::vector<T> range(T min, T max)
+{
+	// create a vector equivalent to the set {min, min+1, ..., max}
+	std::vector<T> r(round(max + 1 - min));
+	std::generate(r.begin(), r.end(), [n = min]() mutable { return n++; });
+	return r;
+}
+
+template <typename T>
+std::vector<T> range(T max)
+{
+	return range(0, max);
+}
+
+template <typename T>
 T random_range(T min, T max)
 {
 	// in range of [min, max]
@@ -25,6 +40,15 @@ template <typename T>
 T random_choice(const std::vector<T>& vector)
 {
 	return vector[random_range<std::size_t>(0, vector.size() - 1)];
+}
+
+template <typename T>
+std::vector<T> random_choices(const std::vector<T>& vector, T k)
+{
+	// select k random values in vector
+	std::vector<T> choices(k);
+	std::generate(choices.begin(), choices.end(), [&]() { return random_choice(vector); });
+	return choices;
 }
 
 template <typename T>
