457c22eef02bccc35cdd7b1415e12c4248769542
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-03-19 21:39:17 +0100

    Add verbose flag and a progressbar in make. Started working on img2ascii function

--- gen_data.py
+++ gen_data.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-PKMN Utility Tool (C) <dev.hentai-chan@outlook.com>
+PKMN Utility Tool (C) hentai-chan <dev.hentai-chan@outlook.com>
 
 Description
 -----------
@@ -25,13 +25,33 @@ import click
 import requests
 from click import style
 from rich.console import Console
+from rich.progress import track
+from PIL import Image
+
+#region Image Processing
+
+def resize_image(img: Image, new_width=15) -> Image:
+    width, height = img.size
+    aspect_ratio = height / float(width)
+    new_height = int(aspect_ratio * new_width)
+    return img.resize((new_width, new_height))
+
+def img2ascii(img: Image, width=15) -> str:
+    # add grey filter
+    img = img.convert('L')
+    img = resize_image(img, new_width=width)
+    # convert pixels to chars
+    img_pixels = list(img.get_data())
+
+#endregion
 
 CONTEXT_SETTINGS = dict(max_content_width=120)
 
 @click.group(invoke_without_command=True, context_settings=CONTEXT_SETTINGS, help=style("PKMN utility tool.", fg='bright_magenta'))
-@click.version_option(version='0.0.1', prog_name="get-data", help=style("Show the version and exit.", fg='yellow'))
+@click.option('--verbose', is_flag=True, default=False, help=style("Enable verbose terminal output.", fg='bright_yellow'))
+@click.version_option(version='0.0.1', prog_name="get-data", help=style("Show the version and exit.", fg='bright_yellow'))
 @click.pass_context
-def cli(ctx):
+def cli(ctx, verbose):
     ctx.ensure_object(dict)
     assets = Path('assets/')
     assets.mkdir(parents=True, exist_ok=True)
@@ -39,6 +59,7 @@ def cli(ctx):
     ctx.obj['BASE_API'] = "https://pokeapi.co/api/v2/pokemon/"
     ctx.obj['SPRITE_API'] = "https://pokeres.bastionbot.org/images/pokemon/"
     ctx.obj['CONSOLE'] = Console()
+    ctx.obj['VERBOSE'] = verbose
 
 @cli.command(context_settings=CONTEXT_SETTINGS, help=style("Create a new PKMN data file.", fg='bright_green'))
 @click.option('--name', type=click.STRING, help=style("Name of a pokemon (English).", fg='bright_yellow'))
@@ -47,40 +68,43 @@ def cli(ctx):
 def make(ctx, name, id):
     result = None
     query = name or id
+    console = ctx.obj['CONSOLE']
 
-    with ctx.obj['CONSOLE'].status('Collecting game data, this may take a while . . .', spinner='dots3') as _:
+    with console.status('Making initial request . . .', spinner='dots3') as _:
         response = requests.get(urljoin(ctx.obj['BASE_API'], query)).json()
-
-        # formula for hp is a rough estimation
         level = randint(30, 60)
         result = {
             'id': response['id'],
             'name': response['name'],
             'level': level,
-            'hp': int(2 * level * (0.8 + 0.4 * random())),
+            'hp': int(2 * level * (0.8 + 0.4 * random())), # formula for hp is a rough estimation
             'atk': 1, 
             'def': 1, 
         }
 
-        # the base api does not provide detailed information about moves, 
-        # so we need to make more calls to the api (+1 per move)
-        moves = {}
-        for index, endpoint in enumerate((move['move']['url'] for move in response['moves'])):
-            move_response = requests.get(endpoint).json()
-            moves[index] = {
-                'name': move_response['names'][7]['name'],
-                'accuracy': int(move_response['accuracy']) if move_response['accuracy'] is not None else None,
-                'effect_changes': move_response['effect_changes'],
-                'effect_chance': int(move_response['effect_chance']) if move_response['effect_chance'] is not None else None,
-                'power': int(move_response['power']) if move_response['power'] is not None else None,
-                'flavor_text_entries': [entry['flavor_text'] for entry in move_response['flavor_text_entries'] if entry['language']['name'] == 'en']
-            }
-        result['moves'] = moves
+    # the base api does not provide detailed information about moves, 
+    # so we need to make more calls to the api (+1 per move)
+    moves = {}
+    endpoints = [move['move']['url'] for move in response['moves']]
+    for index, endpoint in track(enumerate(endpoints), "Sending Requests", total=len(endpoints), transient=True):
+        move_response = requests.get(endpoint).json()
+        moves[index] = {
+            'name': move_response['names'][7]['name'],
+            'accuracy': int(move_response['accuracy']) if move_response['accuracy'] is not None else None,
+            'effect_changes': move_response['effect_changes'],
+            'effect_chance': int(move_response['effect_chance']) if move_response['effect_chance'] is not None else None,
+            'power': int(move_response['power']) if move_response['power'] is not None else None,
+            'flavor_text_entries': [entry['flavor_text'] for entry in move_response['flavor_text_entries'] if entry['language']['name'] == 'en']
+        }
+    result['moves'] = moves
 
-        with open(ctx.obj['ASSETS'].joinpath(f"{result['name']}.json"), mode='w', encoding='utf-8') as file_handler:
-            json.dump(result, file_handler)
+    with open(ctx.obj['ASSETS'].joinpath(f"{result['id']}.json"), mode='w', encoding='utf-8') as file_handler:
+        json.dump(result, file_handler)
 
-    click.secho(f"Done! A new game file was created in '{ctx.obj['ASSETS']}/'.", fg='bright_green')
+    if ctx.obj['VERBOSE']:
+        console.print(result)
+
+    click.secho(f"Done! A new JSON file was created in '{ctx.obj['ASSETS']}/'.", fg='bright_yellow')
 
 @cli.command(context_settings=CONTEXT_SETTINGS, help=style("Create an ASCII image.", fg='bright_green'))
 @click.option('--name', type=click.STRING, help=style("Name of a pokemon (English).", fg='bright_yellow'))
@@ -97,9 +121,18 @@ def ascii(ctx, name, id):
         if name:
             query = requests.get(urljoin(ctx.obj['BASE_API'], query)).json()['id']
         
-        # TODO: implement ascii function
-        image = f"{query}.png"
-        response = requests.get(urljoin(ctx.obj['SPRITE_API'], image))
+        # first find and download the pokemon sprite
+        filename = f"{query}.png"
+        response = requests.get(urljoin(ctx.obj['SPRITE_API'], filename), stream=True)
+        with open(ctx.obj['ASSETS'].joinpath(filename), mode='wb') as file_handler:
+            for chunk in response.iter_content(1024):
+                file_handler.write(chunk)
+
+        # then generate the ascii image
+        pass
+
+    if ctx.obj['VERBOSE']:
+        click.echo("TODO: Preview comes here.")
 
 if __name__ == '__main__':
     try:
