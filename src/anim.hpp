419e64d5daa4fb276eb5c69eff1f673844c3e771
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-05-21 23:17:51 +0200

    Implement load_sprite

--- src/anim.hpp
+++ src/anim.hpp
@@ -4,3 +4,5 @@
 #include<vector>
 
 std::vector<std::string> gen_healthbar(std::string name, int level, int hp, int max_hp);
+
+std::vector<std::string> load_sprite(int id, const std::filesystem::path& assets);
