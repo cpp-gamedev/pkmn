f3f2223af286d447af3dd9bc0e712c6adb352c55
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-05-26 20:52:03 +0200

    Update signature of gen_healthbar, implement print_frame and fix padding issues.

--- src/anim.cpp
+++ src/anim.cpp
@@ -1,42 +1,65 @@
+#include <algorithm>
 #include <filesystem>
 #include <iostream>
 #include <string>
 #include <vector>
-#include "str_format.hpp"
+#include "models.hpp"
 #include "utils.hpp"
+#include "str_format.hpp"
 
-// TODO: update args to Pokemon object
-std::vector<std::string> gen_healthbar(std::string name, int level, int hp, int max_hp)
+std::vector<std::string> gen_healthbar(Pokemon& pkmn)
 {
 	/*
-	 * Bulbasaur :L30	// label
+	 * bulbasaur :L30	// label
 	 * HP [********* ]	// progressbar
 	 *         58 / 60	// hitpoints
 	 */
-	std::string label = kt::format_str("{} :L{}\n", name, level);
+	std::string label = kt::format_str("{} :L{}", pkmn.name, pkmn.level);
+	std::size_t max_width = pkmn.name.length() > 10 ? label.length() : 15;
 
-	double percentage = ceil(static_cast<double>(hp) / max_hp * 100);
+	label = std::string(max_width - label.length(), ' ').append(label);
+
+	double percentage = ceil(static_cast<double>(pkmn.hp) / pkmn.max_hp * 100);
 	int digits = static_cast<int>(log10(percentage));
-	int first_digit = static_cast<int>(percentage / pow(10, digits));
+	int first_digit = (pkmn.max_hp == pkmn.hp) ? 10 : static_cast<int>(percentage / pow(10, digits));
 
 	std::string stars = std::string(first_digit, '*');
 	stars = stars.append(std::string(10LL - first_digit, ' '));
-	std::string progressbar = kt::format_str("HP [{}]\n", style(stars, Color::GREEN));
+	std::string progressbar = kt::format_str("HP [{}]", style(stars, Color::GREEN));
+	progressbar = std::string(max_width - 15, ' ').append(progressbar);
 
-	std::string hitpoints = kt::format_str("{} / {}\n", hp, max_hp);
-	hitpoints = std::string(16 - hitpoints.length(), ' ').append(hitpoints);
+	std::string hitpoints = kt::format_str("{} / {}", pkmn.hp, pkmn.max_hp);
+	hitpoints = std::string(max_width - hitpoints.length(), ' ').append(hitpoints);
 
 	return {label, progressbar, hitpoints};
 }
 
-std::vector<std::string> load_sprite(int id, const std::filesystem::path& assets)
+void print_frame(Pokemon& pkmn1, Pokemon& pkmn2)
 {
-	for (const auto& file : std::filesystem::directory_iterator(assets))
+	std::string healthbars{};
+	std::string sprites{};
+
+	std::vector<std::string> healthbar1 = gen_healthbar(pkmn1);
+	std::vector<std::string> healthbar2 = gen_healthbar(pkmn2);
+
+	std::size_t padding = healthbar1[0].length() + healthbar2[0].length();
+
+	if (padding > 30)
+		padding -= 2*(padding - 30);
+
+	for (std::size_t i = 0; i < 3; i++)
+		healthbars.append(healthbar1[i]).append(std::string(padding, ' ')).append(healthbar2[i]).append("\n");
+
+	std::cout << healthbars;
+
+	for (std::size_t i = 0; i < 20; i++)
 	{
-		if (file.path().filename() == kt::format_str("{}.txt", std::to_string(id)))
-		{
-			return read_file(file);
-		}
+		std::string tmp = pkmn1.sprite[i]; // strip new line
+		tmp.erase(std::remove(tmp.begin(), tmp.end(), '\n'), tmp.end());
+		sprites.append(tmp).append(std::string(20, ' ')).append(pkmn2.sprite[i]);
 	}
-	return {};
+
+	std::cout << sprites;
+
+	return;
 }
