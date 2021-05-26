bb086570c899511f050cde5539a834e9d0cb04bb
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-05-26 20:49:04 +0200

    Populate this->all_moves and implement configure_move_set

--- src/models.cpp
+++ src/models.cpp
@@ -6,15 +6,42 @@
 #include "str_format.hpp"
 #include <dumb_json/djson.hpp>
 
-Move::Move()
+Move::Move(std::string name, MoveType type, int accuracy, int effect_chance, int power, std::string flavor_text)
+	: name(name), type(type), accuracy(accuracy), effect_chance(effect_chance), power(power), flavor_text(flavor_text)
 {
-
 }
 
 std::vector<std::string> Pokemon::read_asset(std::string ext)
 {
 	auto path = assets_dir / std::filesystem::path(kt::format_str("{}.{}", std::to_string(this->id), ext));
 	return read_file(path);
+}
+
+void Pokemon::configure_move_set()
+{
+	this->move_set = random_choices(this->all_moves, 4);
+
+	for (Move& move : this->move_set)
+	{
+		if (move.power > 0 && move.accuracy > 50)
+		{
+			move.type = MoveType::ATTACK;
+			move.flavor_text = kt::format_str("{} deals {} points in damage.", move.name, move.power);
+		}
+		else if (move.power <= 20 && move.effect_chance >= 0)
+		{
+			move.type = random_choice(std::vector<MoveType>{MoveType::BOOST_ATK, MoveType::BOOST_DEF});
+			move.flavor_text = kt::format_str("Boost your {} by 10%.", move.type == MoveType::BOOST_ATK ? "ATK" : "DEF");
+		}
+		else
+		{
+			move.type = MoveType::HEAL;
+			move.accuracy = 100;
+			int heal = random_range<int>(30, 90);
+			move.power = heal + 10 - (heal % 10 == 0 ? 10 : heal % 10); // round to nearest 10
+			move.flavor_text = kt::format_str("Increase your HP by {} points.", move.power);
+		}
+	}
 }
 
 Pokemon::Pokemon(int id, std::filesystem::path assets_dir) : id{id}, assets_dir{assets_dir}
@@ -26,12 +53,32 @@ Pokemon::Pokemon(int id, std::filesystem::path assets_dir) : id{id}, assets_dir{
 	{
 		// pkmn data
 		dj::node_t& node = *n;
+		this->id = id;
 		this->name = node["name"].as<std::string>();
+		this->level = node["level"].as<int>();
 		this->hp = node["hp"].as<int>();
 		this->max_hp = this->hp;
 		this->atk = node["atk"].as<int>();
 		this->def = node["def"].as<int>();
 		// move data
-		// TODO
+		if (auto moves = node.find("moves"))
+		{
+			for (auto const& [num, m] : moves->as<dj::map_nodes_t>())
+			{
+				dj::node_t& move = *m;
+
+				this->all_moves.push_back(
+					Move(
+						move["name"].as<std::string>(),
+						MoveType::NONE,
+						move["accuracy"].as<int>(),
+						move["effect_chance"].as<int>(),
+						move["power"].as<int>(),
+						random_choice(move["flavor_text_entries"].as<std::vector<std::string>>())
+					)
+				);
+			}
+		}
+		this->configure_move_set();
 	}
 }
--- src/models.hpp
+++ src/models.hpp
@@ -8,31 +8,37 @@ enum class MoveType
 {
 	NONE,
 	ATTACK,
-	DEFEND,
 	HEAL,
+	BOOST_ATK,
+	BOOST_DEF
 };
 
 struct Move
 {
 	std::string name{};
-	MoveType move_type{MoveType::NONE};
+	MoveType type{MoveType::NONE};
 	int accuracy{};
-	int damage{};
-	Move();
+	int power{};
+	int effect_chance{};
+	std::string flavor_text{};
+	Move() = default;
+	Move(std::string name, MoveType move_type, int accuracy, int effect_chance, int power, std::string flavor_text);
 };
 
 struct Pokemon
 {
   private:
 	std::vector<std::string> read_asset(std::string ext);
+	void configure_move_set();
 	
 	std::string json{};
-	std::vector<std::string> sprite{};
+	std::vector<Move> all_moves{};
 
   public:
 	std::filesystem::path assets_dir{};
+	std::vector<std::string> sprite{};
 
-	const int id{};
+	int id{};
 	std::string name{};
 	int level{};
 	int hp{};
